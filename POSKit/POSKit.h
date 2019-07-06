53e8d2bf22ab1fca9bce0c6a86205e6d3b5cb8de
Blair Mitchelmore <blair@mitchelmore.ca>
2019-07-06 13:58:06 -0400

    Add POSKit framework

---
+++ POSKit/POSKit.h
@@ -0,0 +1,19 @@
+//
+//  POSKit.h
+//  POSKit
+//
+//  Created by Blair Mitchelmore on 2019-07-06.
+//  Copyright © 2019 TouchBistro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for POSKit.
+FOUNDATION_EXPORT double POSKitVersionNumber;
+
+//! Project version string for POSKit.
+FOUNDATION_EXPORT const unsigned char POSKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <POSKit/PublicHeader.h>
+
+
